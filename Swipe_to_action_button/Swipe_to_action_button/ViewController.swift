49694ca13c7500af320efca61dae589719a997ca
Viktor Holovach <viktorgolovach@Viktors-MacBook-Pro.local>
2023-10-12 10:30:41 +0200

    initial commit

---
+++ Swipe_to_action_button/Swipe_to_action_button/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swipe_to_action_button
+//
+//  Created by Viktor Golovach on 12.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
